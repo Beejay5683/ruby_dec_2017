02ef3d1231d2c1aa6b09923cc4052abb2873b214
Brittney <b.nychole87@gmail.com>
2017-12-07 11:43:23 -0600

    bank account

--- brittney_jackson/oop/bankaccount.rb
+++ brittney_jackson/oop/bankaccount.rb
@@ -1,19 +1,61 @@
 
-
 class BankAccount
-  # your code here
-  def initialize
-  	@actnum
-  	@chkbal
-  	@savbal
-  	@totbal = @chkbal + @savbal
+    attr_reader :account_number, :checking, :saving
 
+    @@bank_accounts = 0
 
+    def initialize 
+        @account_number = create_account
+        @checking = 0
+        @saving = 0
+        @@bank_accounts += 1
+        @interest_rate = 0.01
+    end
 
-  end
+    def deposit(account, amount)
+        if account.downcase == "checking"
+        @checking += amount
+        else
+        @saving += amount 
+        end
+    end
 
-  def
+    def withdrawal(account, amount)
+        if account.downcase == "checking"
+            if @checking - amount < 0
+                raise "Insufficient Funds, you have #{@checking} in this account!!!"
+            else
+                @checking -= amount
+            end
+        else
+            if @saving - amount < 0
+                raise "Insufficient Funds, you have #{@saving} in this account!!!"
+            else
+                @saving -= amount
+            end
+        end
+    end
 
+    def total
+        p "Checking Balance: #{@checking} | Saving Balance: #{@saving} | Total Balance: #{@checking + @saving}"
+    end
 
+    def account_information
+        p "Account Number: #{@account_number}\nInterest Rate: #{@interest_rate}\n#{self.total}" 
+    end
 
+    def self.number_of_accounts
+        @@bank_accounts
+    end
+
+    private
+        def create_account
+            Array.new(10).map { rand(1..9) }.join
+        end
 end
+
+staci = BankAccount.new
+staci.account_number
+staci.deposit('checking', 5000)
+staci.deposit('saving', 5000)
+staci.total
