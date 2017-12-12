c1bb93a67e1191c3a99bd1b97b478b5f8bbbcf97
Brittney <b.nychole87@gmail.com>
2017-12-11 22:02:27 -0600

    updated

--- brittney_jackson/rails2/userAssignment/app/controllers/userss_controller.rb
+++ brittney_jackson/rails2/userAssignment/app/controllers/userss_controller.rb
@@ -23,6 +23,26 @@ class UserssController < ApplicationController
 
   end
 
+  def edit
+  	@user = User.find(params[:id])
+
+  end
+
+  def update
+  	edit = User.find(params[:id]).update(user_params)
+  	edit.save
+
+  	# p user_params
+
+  	# if edit.valid?
+  	# 	edit.save
+  	# 	return redirect_to userss_path user.id 
+  	# elsif
+  	#   	flash[:errors] = user.errors.full_messages
+  	# 	return redirect_to :back
+  	# end
+  end
+
   private
   	def user_params
   		params.require(:userss).permit(:name, :email, :password, :password_confirmation)
